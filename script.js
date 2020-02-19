2c3b90a96cf8f70a5e317b9049814684d9baf002
Graeme Clark <grae0026@stud.kea.dk>
2020-02-19 18:38:53 +0100

    Add RGB section

--- script.js
+++ script.js
@@ -2,27 +2,34 @@
 
 window.addEventListener("DOMContentLoaded", getColor);
 
-const log = document.getElementById("theLogBit");
+const logHex = document.getElementById("theLogBit");
+const logRGB = document.getElementById("theRGBLog");
 
 // function to get color
 function getColor() {
-  console.log("getColor");
-  //   let initColor = document.getElementById("picker").value;
+  //   console.log("getColor");
   let theColor = document.getElementById("picker").addEventListener("input", displayColor);
 }
 
 // function to display color
 function displayColor(e) {
-  console.log("displayColor");
-  log.textContent = e.target.value;
+  //   console.log("displayColor");
+  logHex.textContent = e.target.value;
+}
+
+// function to convert to RGB
+function convertRGB() {
+  console.log("convertRGB");
+  let theColor = document.getElementById("picker").addEventListener("input", displayRGB);
+}
+
+// function to display RGB
+function displayRGB(e) {
+  console.log("displayRGB");
+  logRGB.textContent = e.target.value;
 }
 
 getColor();
-
-// console.log(getColor());
-
-// function to convert to HEX
-
-// Function to convert to RGB
+convertRGB();
 
 // Function to convert to HSL
