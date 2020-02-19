2cc073f98ab49cf81216fff13e5200dba0250f74
Graeme Clark <grae0026@stud.kea.dk>
2020-02-19 22:47:31 +0100

    Converting to RGB with substring

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 window.addEventListener("DOMContentLoaded", getColor);
 
+// console.log(alert(0xee));
+
 const logHex = document.getElementById("theHEXLog");
 const logRGB = document.getElementById("theRGBLog");
 const logHSL = document.getElementById("theHSLLog");
@@ -32,7 +34,14 @@ function centerBoxColor() {
 // function to convert to RGB values
 function convertRGB() {
   console.log("convertRGB");
-  const chosenColor = document.getElementById("picker").addEventListener("input", displayRGB);
+  document.getElementById("picker").addEventListener("input", function() {
+    const hexCode = this.value;
+    const convertToString = `${hexCode}`;
+    const r = convertToString.substring(1, 3);
+    console.log(r);
+    const g = convertToString.substring(3, 5);
+    const b = convertToString.substring(5);
+  });
 }
 
 // function to display RGB values
@@ -45,7 +54,46 @@ function displayRGB(e) {
 // function to convert to HSL values
 function convertHSL() {
   console.log("convertHSL");
-  const chosenColor = document.getElementById("picker").addEventListener("input", displayHSL);
+  // const chosenColor = document.getElementById("picker").addEventListener("input", displayHSL);
+  let r = 200;
+  let g = 200;
+  let b = 100;
+
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  let h, s, l;
+
+  const min = Math.min(r, g, b);
+  const max = Math.max(r, g, b);
+
+  if (max === min) {
+    h = 0;
+  } else if (max === r) {
+    h = 60 * (0 + (g - b) / (max - min));
+  } else if (max === g) {
+    h = 60 * (2 + (b - r) / (max - min));
+  } else if (max === b) {
+    h = 60 * (4 + (r - g) / (max - min));
+  }
+
+  if (h < 0) {
+    h = h + 360;
+  }
+
+  l = (min + max) / 2;
+
+  if (max === 0 || min === 1) {
+    s = 0;
+  } else {
+    s = (max - l) / Math.min(l, 1 - l);
+  }
+  // multiply s and l by 100 to get the value in percent, rather than [0,1]
+  s *= 100;
+  l *= 100;
+
+  // console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
 }
 
 // function to display HSL values
