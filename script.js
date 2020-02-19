0d224b1f374e080570c3a47333747d596d294221
Graeme Clark <grae0026@stud.kea.dk>
2020-02-19 11:07:11 +0100

    Setup color input

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+// function to get color
+
+// function to convert to HEX
+
+// Function to convert to RGB
+
+// Function to convert to HSL
