c067d2d49f5f64c79b6af3725dc63611a67ce560
Graeme Clark <grae0026@stud.kea.dk>
2020-02-19 21:32:41 +0100

    Fix center box color to match chosen color

--- script.js
+++ script.js
@@ -4,39 +4,41 @@ window.addEventListener("DOMContentLoaded", getColor);
 
 const logHex = document.getElementById("theHEXLog");
 const logRGB = document.getElementById("theRGBLog");
+const logHSL = document.getElementById("theHSLLog");
 
 // function to get color from picker
 function getColor() {
-  //   console.log("getColor");
-  let theColor = document.getElementById("picker").addEventListener("input", displayColor);
-}
-
-// function to display chosen color in center box
-function makeColor() {
-  console.log("addColor");
-  const getBox = document.getElementById("thirdBox");
-  getBox.style.setProperty("--third-box-color", "red");
+  console.log("getColor");
+  const theColor = document.getElementById("picker").addEventListener("input", displayColor);
 }
 
 // function to display color as HEX code
 function displayColor(e) {
-  // console.log("displayColor");
+  console.log("displayColor");
   logHex.textContent = e.target.value;
+  centerBoxColor();
+}
+
+// function to display chosen color in center box
+function centerBoxColor() {
+  console.log("centerBoxColor");
+  const getBox = document.getElementById("thirdBox");
+  document.getElementById("picker").addEventListener("input", function() {
+    document.documentElement.style.setProperty("--third-box-color", this.value);
+  });
 }
 
 // function to convert to RGB values
 function convertRGB() {
   // console.log("convertRGB");
-  let theColor = document.getElementById("picker").addEventListener("input", displayRGB);
+  const theColor = document.getElementById("picker").addEventListener("input", displayRGB);
 }
+convertRGB();
 
 // function to display RGB values
 function displayRGB(e) {
   // console.log("displayRGB");
   logRGB.textContent = e.target.value;
 }
-
-getColor();
-convertRGB();
 
 // Function to convert to HSL
