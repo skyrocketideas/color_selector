289dd14e51345cd6bc1dd27894aa9a3e94464a4a
Graeme Clark <grae0026@stud.kea.dk>
2020-02-19 20:28:26 +0100

    Try to display chosen color in center box

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 window.addEventListener("DOMContentLoaded", getColor);
 
-const logHex = document.getElementById("theLogBit");
+const logHex = document.getElementById("theHEXLog");
 const logRGB = document.getElementById("theRGBLog");
 
 // function to get color from picker
@@ -11,21 +11,28 @@ function getColor() {
   let theColor = document.getElementById("picker").addEventListener("input", displayColor);
 }
 
+// function to display chosen color in center box
+function makeColor() {
+  console.log("addColor");
+  const getBox = document.getElementById("thirdBox");
+  getBox.style.setProperty("--third-box-color", "red");
+}
+
 // function to display color as HEX code
 function displayColor(e) {
-  //   console.log("displayColor");
+  // console.log("displayColor");
   logHex.textContent = e.target.value;
 }
 
 // function to convert to RGB values
 function convertRGB() {
-  console.log("convertRGB");
+  // console.log("convertRGB");
   let theColor = document.getElementById("picker").addEventListener("input", displayRGB);
 }
 
 // function to display RGB values
 function displayRGB(e) {
-  console.log("displayRGB");
+  // console.log("displayRGB");
   logRGB.textContent = e.target.value;
 }
 
