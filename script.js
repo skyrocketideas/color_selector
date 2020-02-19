b854de6c06ec415bb36c730f7fcdc7f822f211bf
Graeme Clark <grae0026@stud.kea.dk>
2020-02-19 12:08:28 +0100

    Grab color and display HEX

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", init);
+window.addEventListener("DOMContentLoaded", getColor);
+
+const log = document.getElementById("theLogBit");
 
 // function to get color
-`
+function getColor() {
+  console.log("getColor");
+  //   let initColor = document.getElementById("picker").value;
+  let theColor = document.getElementById("picker").addEventListener("input", displayColor);
+}
+
+// function to display color
+function displayColor(e) {
+  console.log("displayColor");
+  log.textContent = e.target.value;
+}
+
+getColor();
+
+// console.log(getColor());
 
 // function to convert to HEX
 
