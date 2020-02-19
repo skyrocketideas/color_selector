8f9d0528de52e471e6b36af23443b2a65ba953dd
Graeme Clark <grae0026@stud.kea.dk>
2020-02-19 19:26:28 +0100

    add comments

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ window.addEventListener("DOMContentLoaded", getColor);
 const logHex = document.getElementById("theLogBit");
 const logRGB = document.getElementById("theRGBLog");
 
-// function to get color
+// function to get color from picker
 function getColor() {
   //   console.log("getColor");
   let theColor = document.getElementById("picker").addEventListener("input", displayColor);
 }
 
-// function to display color
+// function to display color as HEX code
 function displayColor(e) {
   //   console.log("displayColor");
   logHex.textContent = e.target.value;
 }
 
-// function to convert to RGB
+// function to convert to RGB values
 function convertRGB() {
   console.log("convertRGB");
   let theColor = document.getElementById("picker").addEventListener("input", displayRGB);
 }
 
-// function to display RGB
+// function to display RGB values
 function displayRGB(e) {
   console.log("displayRGB");
   logRGB.textContent = e.target.value;
